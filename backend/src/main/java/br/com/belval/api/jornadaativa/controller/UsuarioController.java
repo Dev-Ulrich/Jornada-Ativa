9e0438090ca425360abec0f968d66b8d53637156
Victor Ulrich <victorulrich07@gmail.com>
2025-06-04 16:57:23 -0300

    Ajustes no Usuario Controller, applications properties, pasta de uploads

--- backend/src/main/java/br/com/belval/api/jornadaativa/controller/UsuarioController.java
+++ backend/src/main/java/br/com/belval/api/jornadaativa/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package br.com.belval.api.jornadaativa.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.belval.api.jornadaativa.model.Usuarios;
 import br.com.belval.api.jornadaativa.repository.UsuarioRepository;
 
 @RestController
 public class UsuarioController {
-
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
     @Autowired
     private UsuarioRepository repository;
 
@@ -45,15 +51,44 @@ public class UsuarioController {
     }
 
     @PostMapping("/usuarios")
-    public ResponseEntity<Usuarios> criarUsuario(@RequestBody Usuarios usuario) {
-        System.out.println("Usuário criado..." + usuario.toString());
-        repository.save(usuario);
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(usuario);
+public ResponseEntity<?> criarUsuario(
+    @RequestParam("nome") String nome,
+    @RequestParam("email") String email,
+    @RequestParam("senha") String senha,
+    @RequestParam(value = "files", required = false) MultipartFile file // Altere para MultipartFile file
+) {
+     logger.info("Recebendo requisição para criar usuário com nome: " + nome + ", email: " + email + ", arquivo: " + (file != null ? file.getOriginalFilename() : "null"));
+    Usuarios usuario = new Usuarios();
+    usuario.setNome(nome);
+    usuario.setEmail(email);
+    usuario.setSenha(senha);
+    
+    String uploadDir = "E:/Jornada-Ativa/backend/uploads/"; // Declaração da variável uploadDir
+    String fileName = null; // Declaração da variável fileName
+    if (file != null && !file.isEmpty()) { // Altere a condição para verificar se o arquivo não é nulo e não está vazio
+        fileName = file.getOriginalFilename();
+        File uploadPath = new File(uploadDir);
+        if (!uploadPath.exists()) {
+            uploadPath.mkdirs();
+        }
+        try {
+    file.transferTo(new File(uploadDir + fileName));
+    logger.info("Arquivo transferido com sucesso: " + fileName); // Adicione esta linha
+    usuario.setFotoPerfil("/uploads/" + fileName);
+} catch (IOException e) {
+    logger.error("Erro ao salvar o arquivo: " + fileName, e);
+    e.printStackTrace();
+}
+    } else {
+        logger.warn("Arquivo recebido está vazio.");
     }
 
+    repository.save(usuario);
+
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(usuario);
+}
     @PutMapping("/usuarios/{id}")
     public ResponseEntity<Object> atualizarUsuario(
             @PathVariable Integer id,
